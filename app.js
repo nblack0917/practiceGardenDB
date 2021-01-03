75b1a6a06c1c83ca37f04b33145786b9a5c12e79
nblack0917 <nickablack@gmail.com>
2021-01-03 09:54:35 -0600

    changed port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use('/plants', plantsRouter)
 
 
-const port = 3000;
+const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
